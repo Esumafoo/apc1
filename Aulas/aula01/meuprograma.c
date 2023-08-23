300ac42dad39719bce6e9564b0e856b038955c27
Esumafoo <129287464+Esumafoo@users.noreply.github.com>
2023-08-23 13:56:07 +0000

    envio para o github

---
+++ Aulas/aula01/meuprograma.c
@@ -0,0 +1,16 @@
+/********************************
+  Autor: Eduardo Ferreira
+  Data: 23/08/2023
+
+  Programa basico em C
+**********************************/
+#include <stdio.h>
+#include <math.h>
+
+//funcao principal
+int main() {
+  printf("Ola pessoal!\n");
+  printf("Raiz quadrada de 2 é %f\n", sqrt(2));
+
+  return 0;
+}
